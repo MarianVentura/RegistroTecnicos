c7db85e2f6cf713435ef789b1be27b9bcc68d3b0
MarianVentura <marianv1119@gmail.com>
2024-05-15 17:37:22 -0400

    Se puso el filtro #5

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -6,8 +6,9 @@ namespace RegistroTecnicos.Models
     {
         [Key]
         public int TecnicoId { get; set; }
-        public string Nombres { get; set; }
-
+        public string? Nombres { get; set; }
+        [Required(ErrorMessage = "El Campo Descripción es obligatorio")]
+        public string? Descripcion { get; set; }
         public decimal SueldoHora { get; set; }
     }
 }
