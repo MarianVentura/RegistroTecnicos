9f15e7c632234971316d17a7be7738f69863e2f3
MarianVentura <marianv1119@gmail.com>
2024-08-29 16:30:27 -0400

    Cree el Modelo Tecnicos.cs en Models Cree y actualice la base de datos Cree el contexto y el Conection String

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "El campo no esta lleno")]
+        public string? Nombre { get; set; }
+
+        public int SueldoHora { get; set; }
+    }
+}
