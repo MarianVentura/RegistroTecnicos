c9ad118a2fcbc910c5ea57c9c8eb7c7801a9fa12
MarianVentura <marianv1119@gmail.com>
2024-08-30 16:56:17 -0400

    Trabaje el program.cs en donde agregue el builder para Blazor

--- Program.cs
+++ Program.cs
@@ -23,14 +23,16 @@ namespace RegistroTecnicos
 
             builder.Services.AddScoped<TecnicoServices>();
 
-            
+            builder.Services.AddBlazorBootstrap();
+
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
