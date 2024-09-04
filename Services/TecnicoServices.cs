b350c7a035c467af2cb4ef80c0b7b43a509d0292
MarianVentura <marianv1119@gmail.com>
2024-09-04 16:03:33 -0400

    Modifique el Files Scoped Services del Services "TecnicoServices"

--- Services/TecnicoServices.cs
+++ Services/TecnicoServices.cs
@@ -4,76 +4,75 @@ using RegistroTecnicos.DAL;
 using System.Linq;
 using RegistroTecnicos.Models;
 
-namespace RegistroTecnicos.Services
+namespace RegistroTecnicos.Services;
+
+public class TecnicoServices
 {
-    public class TecnicoServices
+    private readonly Contexto Contexto;
+
+    public TecnicoServices(Contexto contexto)
     {
-        private readonly Contexto Contexto;
+        Contexto = contexto;
+    }
 
-        public TecnicoServices(Contexto contexto)
+    //Método Existe
+    public async Task<bool> Existe(int TecnicoId)
+    {
+        return await Contexto.Tecnicos.AnyAsync(t => t.TecnicoId == TecnicoId);
+
+    }
+
+    //Método Insertar
+    private async Task<bool> Insertar(Tecnicos tecnico)
+    {
+        Contexto.Tecnicos.Add(tecnico);
+        return await Contexto.SaveChangesAsync() > 0;
+    }
+
+    // Método Modificar
+
+    private async Task<bool> Modificar(Tecnicos tecnico)
+    {
+        Contexto.Tecnicos.Update(tecnico);
+        var modificado = await Contexto.SaveChangesAsync() > 0;
+        Contexto.Entry(tecnico).State = EntityState.Detached;
+        return modificado;
+    }
+
+    // Método Guardar
+    public async Task<bool> Guardar(Tecnicos tecnico)
+    {
+        if (!await Existe(tecnico.TecnicoId))
+            return await Insertar(tecnico);
+        else
         {
-            Contexto = contexto;
+            return await Modificar(tecnico);
         }
+    }
 
-        //Método Existe
-        public async Task<bool> Existe(int TecnicoId)
-        {
-            return await Contexto.Tecnicos.AnyAsync(t => t.TecnicoId == TecnicoId);
+    // Método Eliminar
 
-        }
+    public async Task<bool> Eliminar(int id)
+    {
+        var Tecnicos = await Contexto.Tecnicos.Where(t => t.TecnicoId == id).ExecuteDeleteAsync();
+        return Tecnicos > 0;
+    }
 
-        //Método Insertar
-        private async Task<bool> Insertar(Tecnicos tecnico)
-        {
-            Contexto.Tecnicos.Add(tecnico);
-            return await Contexto.SaveChangesAsync() > 0;
-        }
+    //Método Buscar
 
-        // Método Modificar
+    public async Task<Tecnicos?> Buscar(int id)
+    {
+        return await Contexto.Tecnicos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.TecnicoId == id);
+    }
 
-        private async Task<bool> Modificar(Tecnicos tecnico)
-        {
-            Contexto.Tecnicos.Update(tecnico);
-            var modificado = await Contexto.SaveChangesAsync() > 0;
-            Contexto.Entry(tecnico).State = EntityState.Detached;
-            return modificado;
-        }
+    //Método Listar
 
-        // Método Guardar
-        public async Task<bool> Guardar(Tecnicos tecnico)
-        {
-            if (!await Existe(tecnico.TecnicoId))
-                return await Insertar(tecnico);
-            else
-            {
-                return await Modificar(tecnico);
-            }
-        }
-
-        // Método Eliminar
-
-        public async Task<bool> Eliminar(int id)
-        {
-            var Tecnicos = await Contexto.Tecnicos.Where(t => t.TecnicoId == id).ExecuteDeleteAsync();
-            return Tecnicos > 0;
-        }
-
-        //Método Buscar
-
-        public async Task<Tecnicos?> Buscar(int id)
-        {
-            return await Contexto.Tecnicos
-                .AsNoTracking()
-                .FirstOrDefaultAsync(t => t.TecnicoId == id);
-        }
-
-        //Método Listar
-
-        public async Task <List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
-        {
-            return await Contexto.Tecnicos
-                .Where(criterio)
-                .ToListAsync();
-        }
+    public async Task <List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
+    {
+        return await Contexto.Tecnicos
+            .Where(criterio)
+            .ToListAsync();
     }
 }
