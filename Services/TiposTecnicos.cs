adb76a852f9abd1bcbd428b30db2a601246bc2f2
MarianVentura <marianv1119@gmail.com>
2024-09-04 17:41:01 -0400

    Cree el Servicio TiposTecnicos

---
+++ Services/TiposTecnicos.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using RegistroTecnicos.DAL;
+using System.Linq;
+using RegistroTecnicos.Models;
+using System.Data.Entity;
+
+namespace RegistroTecnicos.Services;
+
+public class TiposTecnicosServices
+{
+    private readonly Contexto _contexto;
+}
+
+public TiposTecnicosServices(Contexto contexto)
+{
+    _contexto = contexto;
+}
+
+public async Task<List<TiposTecnicos>> Listar(Expression<Func<TiposTecnicos, bool>> criterio)
+{
+    return await _contexto.TiposTecnicos
+        .Where(testc => testc.Activo)
+        .ToListAsync();
+}
+
+public async Task<List<TiposTecnicos>> ObtenetTiposActivos()
+{
+    return await _contexto.TiposTecnicos
+        .Where(t => t.Activo)
+        .ToListAsync();
+}
+
+public async Task<TiposTecnicos> ObtenerPorId(int id)
+{
+    return await _contexto.TiposTecnicos
+        .AsNoTracking()
+        .FirstOrDefaultAsync(t => t.Id == id);
+}
+
