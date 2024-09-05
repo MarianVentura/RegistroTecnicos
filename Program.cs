764766113fab74411376b17d3c53d129eefb4dcd
MarianVentura <marianv1119@gmail.com>
2024-09-05 17:26:27 -0400

    Inyecte el TiposTecnicosServices al program.cs

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ public class Program
         builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
 
         builder.Services.AddScoped<TecnicoServices>();
+        builder.Services.AddScoped<TiposTecnicosServices>();
 
         builder.Services.AddBlazorBootstrap();
 
