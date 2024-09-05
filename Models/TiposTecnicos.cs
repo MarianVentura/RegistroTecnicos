ba81ce8b5c02e53ff61e40cc169b0cf1ff34df6f
MarianVentura <marianv1119@gmail.com>
2024-09-05 16:04:09 -0400

    Cree el modelo TiposTecnicos

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposTecnicos
+{
+    public int TipoTecnicoId { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public bool Activo { get; set; }
+
+   
+}
