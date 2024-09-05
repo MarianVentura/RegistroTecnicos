5457ba518345132848274968d9748856f60aace6
MarianVentura <marianv1119@gmail.com>
2024-09-05 17:26:49 -0400

    Trabaje el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,9 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Tecnicos> Tecnicos { get; set; }
-    public DbSet<TipoTecnico> TipoTecnicos { get; set; } 
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+
+   
 }
+
+
