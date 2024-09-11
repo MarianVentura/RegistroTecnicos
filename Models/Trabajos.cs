436fa73a737d1484c742aead0184d5c7eee1308a
MarianVentura <marianv1119@gmail.com>
2024-09-11 15:26:53 -0400

    Cree y trabaje el modelo Trabajos

---
+++ Models/Trabajos.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class Trabajos
+{
+    [Key]
+    public int TrabajoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public int ClienteId { get; set; }
+
+    [ForeignKey("ClienteId")]
+    public Clientes? Cliente { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public int TecnicoId { get; set; }
+
+    [ForeignKey("TecnicoId")]
+    public Tecnicos? Tecnico { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Monto { get; set; }
+
+    public TimeSpan Tiempo { get; set; } // Tiempo tomado por el técnico
+}
