200548cbeeede1da9d9ac06356db02debbd84acd
MarianVentura <marianv1119@gmail.com>
2024-09-11 15:16:18 -0400

    Cree el modelo Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClientesId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo solo debe contener letras.")]
+    public string? NombresClientes { get; set; }
+
+    [Required(ErrorMessage = "El número de WhatsApp es obligatorio.")]
+    [Phone(ErrorMessage = "El número de WhatsApp no es válido.")]
+    public string? WhatsApp { get; set; }
+
+
+}
