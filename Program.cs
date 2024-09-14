fce3da9e3629252a8201ea522e3f09b0a0535908
MarianVentura <marianv1119@gmail.com>
2024-09-13 23:23:08 -0400

    .

--- Program.cs
+++ Program.cs
@@ -24,8 +24,8 @@ public class Program
         builder.Services.AddScoped<TecnicoServices>();
         builder.Services.AddScoped<TiposTecnicosServices>();
         builder.Services.AddScoped<ClientesServices>();
-        builder.Services.AddHttpClient();
         builder.Services.AddScoped<TrabajosServices>();
+        
 
         builder.Services.AddBlazorBootstrap();
 
