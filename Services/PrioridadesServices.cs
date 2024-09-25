f9451145beb27aa5262952e55ccd88312643036f
MarianVentura <marianv1119@gmail.com>
2024-09-25 12:21:25 -0400

    Modifique el services de Prioridades

--- Services/PrioridadesServices.cs
+++ Services/PrioridadesServices.cs
@@ -28,15 +28,15 @@ public class PrioridadesServices
     private async Task<bool> Modificar(Prioridades prioridad)
     {
         _contexto.Prioridades.Update(prioridad);
-        var modificado = await _contexto.SaveChangesAsync() > 0;
-        _contexto.Entry(prioridad).State = EntityState.Detached;
-        return modificado;
+        return await _contexto.SaveChangesAsync() > 0;
     }
 
     public async Task<bool> Guardar(Prioridades prioridad)
     {
         if (!await Existe(prioridad.PrioridadId))
+        {
             return await Insertar(prioridad);
+        }
         else
             return await Modificar(prioridad);
     }
@@ -59,11 +59,12 @@ public class PrioridadesServices
     public async Task<List<Prioridades>> Listar(Expression<Func<Prioridades, bool>> criterio)
     {
         return await _contexto.Prioridades
+            .AsNoTracking()
             .Where(criterio)
             .ToListAsync();
     }
 
-    public async Task<bool> ExisteDescripcion(int prioridadId, string descripcion)
+    public async Task<bool> ExistePrioridad(int prioridadId, string descripcion)
     {
         return await _contexto.Prioridades
             .AnyAsync(p => p.PrioridadId != prioridadId && p.Descripcion.ToLower().Equals(descripcion.ToLower()));
