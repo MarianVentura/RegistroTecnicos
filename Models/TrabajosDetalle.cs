d4ecac0c6ba610985487da8f729f564afaa51b96
MarianVentura <marianv1119@gmail.com>
2024-10-19 13:32:40 -0400

    Modifique el Modelo TrabajosDetalle

---
+++ Models/TrabajosDetalle.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public int TrabajoId { get; set; }
+
+    [ForeignKey("TrabajoId")]
+    public Trabajos? Trabajo { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public int ArticuloId { get; set; }
+
+    [ForeignKey("ArticuloId")]
+    public Articulos? Articulos { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public int Cantidad {  get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public decimal Precio { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public decimal Costo { get; set; }
+}
