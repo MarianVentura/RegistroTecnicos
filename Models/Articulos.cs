13e2bd8119491b692d227cf59edff7fb6256088a
MarianVentura <marianv1119@gmail.com>
2024-10-19 12:32:39 -0400

    Cree el modelo Articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public decimal Costo { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio.")]
+    public decimal Precio { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public int Existencia { get; set; }
+
+    
+}
