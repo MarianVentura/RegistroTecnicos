0e91cdc5194c628f07a499e409e4317562199c8a
MarianVentura <marianv1119@gmail.com>
2024-10-19 21:15:06 -0400

    Modifique el Modelo Articulos

--- Models/Articulos.cs
+++ Models/Articulos.cs
@@ -1,23 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Timers;
 
 namespace RegistroTecnicos.Models;
 
 public class Articulos
 {
     [Key]
+
+    [Required(ErrorMessage = "El Id debe de ser mayor a 1")]
     public int ArticuloId { get; set; }
 
-    [Required(ErrorMessage = "Campo Obligatorio.")]
-    public string Descripcion { get; set; }
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public string? Descripcion { get; set; }
 
-    [Required(ErrorMessage = "Campo Obligatorio.")]
-    public decimal Costo { get; set; }
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public decimal? Costo { get; set; }
 
-    [Required(ErrorMessage = "Campo Obligatorio.")]
-    public decimal Precio { get; set; }
 
-    [Required(ErrorMessage = "Campo Obligatorio")]
-    public int Existencia { get; set; }
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public decimal? Precio { get; set; }
 
-    
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public decimal? Existencia { get; set; }
+
 }
