74febc2c378bfcc569791409460d3f60e0545985
MarianVentura <marianv1119@gmail.com>
2024-10-19 21:16:01 -0400

    Modifique el ArticulosServices

---
+++ Services/ArticuloServices.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+public class ArticuloServices
+{
+    private readonly Contexto _contexto;
+
+    public ArticuloServices(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public async Task<List<Articulos>> ListaArticulos()
+    {
+        return await _contexto.Articulos
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<Articulos?> ObtenerArticuloPorId(int id)
+    {
+        return await _contexto.Articulos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.ArticuloId == id);
+    }
+
+    public async Task ActualizarExistencia(int articuloId, decimal cantidad)
+    {
+        var articulo = await _contexto.Articulos.FindAsync(articuloId);
+        if (articulo != null)
+        {
+            articulo.Existencia -= cantidad;
+            _contexto.Articulos.Update(articulo);
+            await _contexto.SaveChangesAsync();
+        }
+    }
+
+    public async Task AgregarCantidad(int articuloId, int cantidad)
+    {
+        var articulo = await _contexto.Articulos.FindAsync(articuloId);
+
+        if (articulo != null)
+        {
+            articulo.Existencia += cantidad;
+
+            await _contexto.SaveChangesAsync();
+        }
+    }
+
+}
