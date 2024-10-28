a9e91370c6630b6f1bc55b71345b94d52f4ff481
MarianVentura <marianv1119@gmail.com>
2024-10-28 11:26:05 -0400

    Cree y Trabaje el modelo CotizacionesDetalle

---
+++ Models/CotizacionesDetalle.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+    public class CotizacionesDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public int CotizacionId { get; set; }
+
+        [ForeignKey("CotizacionId")]
+        public Cotizaciones? Cotizacion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public int ArticuloId { get; set; }
+
+        [ForeignKey("ArticuloId")]
+        public Articulos? Articulo { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public decimal Precio { get; set; }
+    }
+}
